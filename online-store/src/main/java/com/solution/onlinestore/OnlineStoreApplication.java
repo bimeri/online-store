6011f71842a383f0669466b91d2455df213d1986
Bimeri Noel <bimerinoel@gmail.com>
2023-05-23 04:10:10 +0100

    remove comments

--- online-store/src/main/java/com/solution/onlinestore/OnlineStoreApplication.java
+++ online-store/src/main/java/com/solution/onlinestore/OnlineStoreApplication.java
@@ -1,10 +1,8 @@
 package com.solution.onlinestore;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableFeignClients
 @SpringBootApplication
 public class OnlineStoreApplication {
 
